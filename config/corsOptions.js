35a87228a6057971a143b4a1074bbaf6c6364d72
Csordas Balazs <csordasbalu96@gmail.com>
2022-11-23 07:40:40 +0100

    edited allowed origins again

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,8 +1,16 @@
 require ('dotenv').config();
 
-const corsOptions = {
-    origin: process.env.CLIENT_URL,
-    optionsSuccessStatus: 200
-}
+const allowedOrigins = [
+    process.env.CLIENT_URL
+];
+
+var corsOptions = function (req, callback) {
+    if (allowedOrigins.indexOf(req.header('Origin')) !== -1) {
+      corsOptions = { origin: true }
+    } else {
+      corsOptions = { origin: false }
+    }
+    callback(null, corsOptions)
+  }
 
 module.exports = corsOptions;
